6d436a9db2f4f8599ba66216eca07948054bdb86
pasha6ko <pasha.kotov123123@gmail.com>
2023-08-14 14:05:56 +0300

    PlayerLook

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -3,22 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerLook : MonoBehaviour
 {
-    [SerializeField] Transform camera;
+    [SerializeField] private Transform camera;
+    [SerializeField] private Transform player;
     [SerializeField] private float rotationSensivity;
     private Vector2 _inputVector;
 
     private void Update()
     {
-        float eulerX = ( -_inputVector.y * rotationSensivity) % 360;
-        print(eulerX);
+        float eulerX = (-_inputVector.y * rotationSensivity) % 360;
         float eulerY = (_inputVector.x * rotationSensivity) % 360;
-        camera.transform.rotation = Quaternion.Euler(Mathf.Clamp(camera.transform.rotation.eulerAngles.x + eulerX, -70, 75), 0, 0);
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + eulerY, 0);
+
+        Vector3 targetYRotation = new Vector3(0, transform.rotation.eulerAngles.y + eulerY, 0);
+        Vector3 targetXRotation = new Vector3(camera.rotation.eulerAngles.x + eulerX, 0, 0);
+        player.rotation = Quaternion.Euler(targetYRotation);
+        camera.localRotation = Quaternion.Euler(targetXRotation);
         _inputVector = Vector2.zero;
     }
     public void OnLook(InputValue input)
     {
-        //print(input.Get<Vector2>());
         _inputVector = input.Get<Vector2>();
     }
 
