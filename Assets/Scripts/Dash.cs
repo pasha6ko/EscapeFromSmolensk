ceaffd1a187b6982acf0f47fc9718a4bdfaa3c16
pasha6ko <pasha.kotov123123@gmail.com>
2023-08-17 14:35:55 +0300

    Dash Refactoring v2

--- Assets/Scripts/Dash.cs
+++ Assets/Scripts/Dash.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Dash : MonoBehaviour
 {
@@ -12,24 +13,36 @@ public class Dash : MonoBehaviour
     [SerializeField] private int dashMaxCount;
     [SerializeField, Range(0, 20)] private float dashRecoverTime, dashForce;
 
-    private int _dashCount;
+    [SerializeField] private int _dashCount;
     private Coroutine _dashRecoverProcess;
+    private Vector2 _inputVector;
+
 
     private void Start()
     {
         _dashCount = dashMaxCount;
     }
+    public void FixedUpdate()
+    {
+        DeshRecover();
+    }
+    public void OnMove(InputValue input)
+    {
+        _inputVector = input.Get<Vector2>();
+    }
     public void OnDash()
     {
         if (_dashCount <= 0) return;
-        Vector3 planeCameraDiraction = new Vector3(cam.forward.x, 0, cam.forward.z).normalized;
-        rb.AddForce(planeCameraDiraction * dashForce, ForceMode.VelocityChange);
+        if (_inputVector == Vector2.zero) _inputVector = Vector2.up; 
+        Vector3 forceDirection = transform.forward * _inputVector.y + transform.right * _inputVector.x;
+        rb.AddForce(forceDirection * dashForce, ForceMode.VelocityChange);
         _dashCount--;
+        DeshRecover();
 
     }
     public void DeshRecover()
     {
-        if (_dashRecoverProcess != null)return;
+        if (_dashRecoverProcess != null) return;
         if (!playerMovement.IsGrounded()) return;
         if (_dashCount >= dashMaxCount) return;
         _dashRecoverProcess = StartCoroutine(DeshRecoverAs());
