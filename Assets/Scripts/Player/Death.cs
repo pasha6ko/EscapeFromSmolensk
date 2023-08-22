f4f64109a23b7454652a35cebaccd19e65382738
buloshka <79216709+buloshka@users.noreply.github.com>
2023-08-22 18:36:20 +0300

    Dead v1

--- Assets/Scripts/Player/Death.cs
+++ Assets/Scripts/Player/Death.cs
@@ -11,20 +11,15 @@ public class Death : MonoBehaviour
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private CheckPoint playerCheckPoint;
     [Header("Death Components")]
-    [SerializeField] private GameObject deathVideo;
     [SerializeField] private GameObject deathUI;
     [HideInInspector] public bool isDead = false;
 
     private IEnumerator Recovery()
     {
         ScoreCounter.Instance.Damage(2);
-        deathVideo.SetActive(true);
         deathUI.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
         isDead = true;
-        yield return new WaitForSeconds(9.5f);
-        OnEKeyClick();
-
+        yield return new WaitForSeconds(1f);
     }
 
     public void StartDeath()
@@ -32,19 +27,5 @@ public class Death : MonoBehaviour
         if (isDead) return;
         StartCoroutine(Recovery());
         Destroy(playerMovement);
-    }
-
-    public void OnEKeyClick()
-    {
-        if (!isDead) return;
-        deathVideo.SetActive(false);
-        deathUI.SetActive(false);
-
-        isDead = false;
-        OnDeath?.Invoke();
-        SwitchOn?.Invoke();
-
-        StopCoroutine(Recovery());
-        Destroy(gameObject);
     }
 }
